d70560f6045870d0a68452bc254abfc6d4b5e820
kioryu <1218moonlight@gmail.com>
2019-02-10 13:24:38 +0900

    최초 Commit 最初のCommit

---
+++ app/src/main/java/com/example/kmoon/my_history/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kmoon.my_history;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
