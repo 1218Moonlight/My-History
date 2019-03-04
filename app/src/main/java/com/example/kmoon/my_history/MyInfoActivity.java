7f15105736b422d3394c1bcc7b445c231e4a6e28
kmoon <1218moonlight@gmail.com>
2019-03-04 18:45:49 +0900

    (MyInfoActivity) -> 날짜 선택 다이어로그로 변경. -> 日にちの選択をDialogに変更。

--- app/src/main/java/com/example/kmoon/my_history/MyInfoActivity.java
+++ app/src/main/java/com/example/kmoon/my_history/MyInfoActivity.java
@@ -1,9 +1,11 @@
 package com.example.kmoon.my_history;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -11,9 +13,11 @@ import android.widget.Toast;
 import com.example.kmoon.my_history.base.DefaultActivity;
 
 public class MyInfoActivity extends DefaultActivity implements View.OnClickListener {
-    protected Button mainOk;
-    protected EditText enterName, enterBirthday;
+    protected Button mainOk, birthBtn;
+    protected EditText enterName;
     protected RadioGroup radioGroup;
+    protected String selectDate = "empty";
+    protected DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,12 +35,12 @@ public class MyInfoActivity extends DefaultActivity implements View.OnClickListe
                 if (this.enterName.getText().toString().equals("")) {
                     Toast.makeText(this, getString(R.string.user_name), Toast.LENGTH_SHORT).show();
                     break;
-                } else if (this.enterBirthday.getText().toString().equals("")) {
+                } else if (this.selectDate.equals("empty")) {
                     Toast.makeText(this, getString(R.string.user_birthday), Toast.LENGTH_SHORT).show();
                     break;
                 }
                 this.infoMap.put("name", this.enterName.getText().toString());
-                this.infoMap.put("birthday", this.enterBirthday.getText().toString());
+                this.infoMap.put("birthday", selectDate);
                 if (this.radioGroup.getCheckedRadioButtonId() == R.id.mainMan) {
                     this.infoMap.put("gender", getString(R.string.user_man));
                 } else if (this.radioGroup.getCheckedRadioButtonId() == R.id.mainWoman) {
@@ -48,6 +52,10 @@ public class MyInfoActivity extends DefaultActivity implements View.OnClickListe
                 Toast.makeText(this, getString(R.string.user_complete), Toast.LENGTH_SHORT).show();
                 this.startActivityIntent(MainActivity.class);
                 break;
+
+            case R.id.mainBirthdayBtn:
+                datePickerDialog.show();
+                break;
         }
     }
 
@@ -55,9 +63,19 @@ public class MyInfoActivity extends DefaultActivity implements View.OnClickListe
         this.mainOk = findViewById(R.id.mainOk);
         this.mainOk.setOnClickListener(this);
 
+        this.birthBtn = findViewById(R.id.mainBirthdayBtn);
+        this.birthBtn.setOnClickListener(this);
+
         this.enterName = findViewById(R.id.mainEnterName);
-        this.enterBirthday = findViewById(R.id.mainEnterBirthday);
 
         this.radioGroup = findViewById(R.id.mainRadioGroup);
+
+        this.datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                selectDate = "" + year + "-" + month + "-" + dayOfMonth;
+                Toast.makeText(getApplicationContext(), year + "년" + month + "월" + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
+            }
+        }, 1988, 1, 1);
     }
 }
