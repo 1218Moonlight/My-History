45365a71a606d73bbaa6fd463b10034296463784
kmoon <1218moonlight@gmail.com>
2019-03-15 19:20:01 +0900

    날짜 계산관련 기능 추가중... (테스트...) 日付計算の機能を追加中。。。（TEST。。。）

--- app/src/main/java/com/example/kmoon/my_history/MainActivity.java
+++ app/src/main/java/com/example/kmoon/my_history/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.kmoon.my_history;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.example.kmoon.my_history.base.DefaultActivity;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends DefaultActivity {
 
@@ -17,21 +18,51 @@ public class MainActivity extends DefaultActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
 
-        if (this.myInfoSp.getPrefer().equals(getString(R.string.default_empty))) {
-            startActivityIntent(MyInfoActivity.class);
-        } else {
-            this.bind();
-            try {
-                this.myinfoJson.init(new JSONObject(this.myInfoSp.getPrefer()));
-                this.name.setText(this.myinfoJson.getString(getString(R.string.user_name)));
-                this.birthday.setText(this.myinfoJson.getString(getString(R.string.user_birthday)));
-                this.gender.setText(this.myinfoJson.getString(getString(R.string.user_gender)));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        Log.d("testtest", "" + test(2016, 10, 27, 2019, 3, 15));
 
-        }
+
+//        if (this.myInfoSp.getPrefer().equals(getString(R.string.default_empty))) {
+//            startActivityIntent(MyInfoActivity.class);
+//        } else {
+//            this.bind();
+//            try {
+//                this.myinfoJson.init(new JSONObject(this.myInfoSp.getPrefer()));
+//                this.name.setText(this.myinfoJson.getString(getString(R.string.user_name)));
+//                this.birthday.setText(this.myinfoJson.getString(getString(R.string.user_birthday)));
+//                this.gender.setText(this.myinfoJson.getString(getString(R.string.user_gender)));
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
+
+    private int test(int year1, int month1, int date1, int year2, int month2, int date2) {
+        Calendar cal = Calendar.getInstance();
+
+        int dateCount1 = 0, dateCount2 = 0, yearDiff = 0, dayDiff = 0;
+        if (year1 > year2) {
+            for (int i = year2; i < year1; i++) {
+                cal.set(i, 12, 0);
+                yearDiff += cal.get(Calendar.DAY_OF_YEAR);
+            }
+            dateCount1 += yearDiff;
+        } else if (year1 < year2) {
+            for (int i = year1; i < year2; i++) {
+                cal.set(i, 12, 0);
+                yearDiff += cal.get(Calendar.DAY_OF_YEAR);
+            }
+            dateCount2 += yearDiff;
+        }
+        cal.set(year1, month1 - 1, date1);
+        dayDiff = cal.get(Calendar.DAY_OF_YEAR);
+        dateCount1 += dayDiff;
+        cal.set(year2, month2 - 1, date2);
+        dayDiff = cal.get(Calendar.DAY_OF_YEAR);
+        dateCount2 += dayDiff;
+        return dateCount1 - dateCount2;
+    }
+
+
 
     private void bind() {
         this.name = findViewById(R.id.main_user_name);
